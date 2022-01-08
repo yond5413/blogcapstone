b0e1bfc4019674997d107cfa610f97c9ace0fac0
yond5413 <87887705+yond5413@users.noreply.github.com>
2022-01-07 22:45:04 -0500

    Update and rename src/main/java/com/mthree/cardealership/TestApplicationConfiguration.java to src/main/java/com/cardealership/TestApplicationConfiguration.java

--- src/main/java/com/cardealership/TestApplicationConfiguration.java
+++ src/main/java/com/cardealership/TestApplicationConfiguration.java
@@ -1,4 +1,4 @@
-package com.mthree.cardealership;
+package com.cardealership;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
