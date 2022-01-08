853b5a42eb94a807c0cbe84b84a6ea42c7751273
yond5413 <87887705+yond5413@users.noreply.github.com>
2022-01-07 22:41:38 -0500

    Update and rename src/main/java/com/mthree/cardealership/entity/User.java to src/main/java/com/cardealership/entity/User.java

--- src/main/java/com/cardealership/entity/User.java
+++ src/main/java/com/cardealership/entity/User.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mthree.cardealership.entity;
+package com.cardealership.entity;
 
 /**
  *
