d32531e27acd120102ba3d0286eb28d3be99185e
yond5413 <87887705+yond5413@users.noreply.github.com>
2022-01-07 22:44:39 -0500

    Update and rename src/main/java/com/mthree/cardealership/App.java to src/main/java/com/cardealership/App.java

--- src/main/java/com/cardealership/App.java
+++ src/main/java/com/cardealership/App.java
@@ -1,4 +1,4 @@
-package com.mthree.cardealership;
+package com.cardealership;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
