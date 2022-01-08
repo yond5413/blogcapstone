3de92f7462af9da4f5a18c4bf1045540edbb42cc
yond5413 <87887705+yond5413@users.noreply.github.com>
2022-01-07 22:35:30 -0500

    Update and rename src/main/java/com/mthree/cardealership/dao/UserDaoDB.java to src/main/java/com/cardealership/dao/UserDaoDB.java

--- src/main/java/com/cardealership/dao/UserDaoDB.java
+++ src/main/java/com/cardealership/dao/UserDaoDB.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mthree.cardealership.dao;
+package com.cardealership.dao;
 
 import com.mthree.cardealership.entity.User;
 import java.sql.ResultSet;
