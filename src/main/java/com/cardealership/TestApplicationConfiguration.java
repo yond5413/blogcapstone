db6baa6eb378f5f35561a17a9cc39c21e02932f2
yond5413 <87887705+yond5413@users.noreply.github.com>
2022-01-07 23:06:25 -0500

    Update TestApplicationConfiguration.java

--- src/main/java/com/cardealership/TestApplicationConfiguration.java
+++ src/main/java/com/cardealership/TestApplicationConfiguration.java
@@ -1,6 +1,6 @@
 package com.cardealership;
 
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.CommandLineRunner; 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
